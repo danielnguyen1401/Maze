2d45ebbf9d0df8802c827b18400c17bf4be6208a
YUJIN\DAN <nguyen.leenguyen@gmail.com>
2019-08-26 17:29:34 +0700

    maze almost done for wall and floor

--- Assets/Scripts/MazeCell.cs
+++ Assets/Scripts/MazeCell.cs
@@ -1,9 +1,36 @@
-﻿using UnityEngine;
+﻿using System;
+using UnityEngine;
+using Random = UnityEngine.Random;
 
 public class MazeCell : MonoBehaviour
 {
     public IntVec coordinates;
+    private int _initEdgeCount;
     private readonly Edge[] edges = new Edge[MazeDirections.Count];
+
+    public bool IsGenerateCompleted => _initEdgeCount == MazeDirections.Count;
+
+    public Direction RandomInitDirection
+    {
+        get
+        {
+            var directionCount = MazeDirections.Count;
+            var skips = Random.Range(0, directionCount - _initEdgeCount);
+            for (var i = 0; i < directionCount; i++)
+            {
+                if (edges[i] == null)
+                {
+                    if (skips == 0)
+                    {
+                        return (Direction) i;
+                    }
+                    skips -= 1;
+                }
+            }
+
+            throw new InvalidOperationException();
+        }
+    }
 
     public Edge GetEdge(Direction direction)
     {
@@ -13,5 +40,6 @@ public class MazeCell : MonoBehaviour
     public void SetEdge(Direction direction, Edge edge)
     {
         edges[(int) direction] = edge;
+        _initEdgeCount += 1;
     }
 }
--- Assets/Scripts/MazeController.cs
+++ Assets/Scripts/MazeController.cs
@@ -2,6 +2,14 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+// <summary>
+// Growing Tree algorithm
+// #1. Let C be a list of cells, initially empty. Add one cell to C, at random.
+// #2. Choose a cell from C, and carve a passage to any unvisited neighbor of that cell, adding that neighbor to C as well.
+//     If there are no unvisited neighbors, remove the cell from C.
+// #3. Repeat #2 until C is empty.
+// http://weblog.jamisbuck.org/2011/1/27/maze-generation-growing-tree-algorithm
+// </summary>
 public class MazeController : MonoBehaviour
 {
     public MazeCell cellPrefab;
@@ -45,45 +53,49 @@ public class MazeController : MonoBehaviour
         var newCell = Instantiate(cellPrefab, transform);
         _cells[coordinates.x, coordinates.z] = newCell;
         newCell.coordinates = coordinates;
-        newCell.name = "cell-" + coordinates.x + "," + coordinates.z;
+        newCell.name = "cell - " + coordinates.x + "," + coordinates.z;
         newCell.transform.localPosition = new Vector3(coordinates.x - mazeSize.x * 0.5f + 0.5f, 0f, coordinates.z - mazeSize.z * 0.5f + 0.5f);
         return newCell;
     }
 
     private void FirstGenerate(List<MazeCell> cells)
     {
-        cells.Add(CreateCell(RandomCoordinates));
+        cells.Add(CreateCell(CoordinatesBeRandom));
     }
 
     private void NextGenerate(List<MazeCell> cells)
     {
         int currentIndex = cells.Count - 1;
         MazeCell currentCell = cells[currentIndex];
-        Direction direction = MazeDirections.RandomValue;
+        if (currentCell.IsGenerateCompleted)
+        {
+            cells.RemoveAt(currentIndex);
+            return;
+        }
+
+        var direction = currentCell.RandomInitDirection;
         IntVec coordinates = currentCell.coordinates + direction.ToIntVector2();
-        if (ContainsCoordinates(coordinates))
+        if (ContainsCoordinate(coordinates))
         {
             MazeCell neighbor = GetCell(coordinates);
             if (neighbor == null)
             {
                 neighbor = CreateCell(coordinates);
-                CreatePassage(currentCell, neighbor, direction);
+                MakePassage(currentCell, neighbor, direction);
                 cells.Add(neighbor);
             }
             else
             {
-                CreateWall(currentCell, neighbor, direction);
-                cells.RemoveAt(currentIndex);
+                MakeWall(currentCell, neighbor, direction);
             }
         }
         else
         {
-            CreateWall(currentCell, null, direction);
-            cells.RemoveAt(currentIndex);
+            MakeWall(currentCell, null, direction);
         }
     }
 
-    private void CreatePassage(MazeCell cell, MazeCell otherCell, Direction direction)
+    private void MakePassage(MazeCell cell, MazeCell otherCell, Direction direction)
     {
         var passage = Instantiate(passagePref);
         passage.Initialize(cell, otherCell, direction);
@@ -91,20 +103,18 @@ public class MazeController : MonoBehaviour
         passage.Initialize(otherCell, cell, direction.GetOpposite());
     }
 
-    private void CreateWall(MazeCell cell, MazeCell otherCell, Direction direction)
+    private void MakeWall(MazeCell cell, MazeCell otherCell, Direction direction)
     {
         var wall = Instantiate(wallPref);
         wall.Initialize(cell, otherCell, direction);
-        if (otherCell != null)
-        {
-            wall = Instantiate(wallPref);
-            wall.Initialize(otherCell, cell, direction.GetOpposite());
-        }
+        if (otherCell == null) return;
+        wall = Instantiate(wallPref);
+        wall.Initialize(otherCell, cell, direction.GetOpposite());
     }
 
-    private IntVec RandomCoordinates => new IntVec(Random.Range(0, mazeSize.x), Random.Range(0, mazeSize.z));
+    private IntVec CoordinatesBeRandom => new IntVec(Random.Range(0, mazeSize.x), Random.Range(0, mazeSize.z));
 
-    private bool ContainsCoordinates(IntVec coordinate)
+    private bool ContainsCoordinate(IntVec coordinate)
     {
         return coordinate.x >= 0 && coordinate.x < mazeSize.x && coordinate.z >= 0 && coordinate.z < mazeSize.z;
     }
